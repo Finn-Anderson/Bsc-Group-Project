f6bdfb647d210dc426237701a0d05d371b6a8733
BscPaul <114180827+BscPaul@users.noreply.github.com>
2023-03-05 21:53:18 +0000

    Update menus.js

--- src/classes/menus.js
+++ src/classes/menus.js
@@ -1,1 +1,25 @@
 
+/*
+    Development build version: 0.1
+    Build date: 02/03/2023
+    Author: Bsc Team Project Group 2023
+    Description: Menu Navigation Components
+*/
+
+AFRAME.registerComponent('menu', {
+    schema: {
+        menu:{type:"string",default:""},
+    },
+
+    init: function() {
+
+        var menu = this.data.menu;
+
+        this.setMenu = function() {
+            console.log(1);
+        }
+
+    },
+    update: function() {},
+    remove: function() {}
+})
