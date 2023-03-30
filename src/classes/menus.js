132b44c02aef9e0a40009b8a80aa6fbd251e75b3
BscPaul <114180827+BscPaul@users.noreply.github.com>
2023-03-30 11:07:44 +0100

    Update menus.js

--- src/classes/menus.js
+++ src/classes/menus.js
@@ -1,25 +1,54 @@
-
 /*
     Development build version: 0.1
     Build date: 02/03/2023
     Author: Bsc Team Project Group 2023
-    Description: Menu Navigation Components
+    Description: Hotpoint Navigation Components
 */
 
-AFRAME.registerComponent('menu', {
+AFRAME.registerComponent('hotpoint', {
     schema: {
-        menu:{type:"string",default:""},
+        room:{type:"string",default:""},
+        points: {type:"string", default:""},
     },
 
     init: function() {
 
-        var menu = this.data.menu;
+        var newSky = this.data.room;
+        var newPoints = this.data.points;
 
-        this.setMenu = function() {
-            console.log(1);
+        this.setSky = function() {
+            var sky = document.querySelector('#img-360');
+            sky.setAttribute("src", newSky);
         }
+
+        this.setHidden = function() {
+            var current_hotpoint = this.parentElement.getAttribute("id");
+            var hotpoint = document.querySelector('#'+current_hotpoint);
+            hotpoint.setAttribute("scale","0 0 0");
+            console.log(current_hotpoint);
+        }
+
+        this.setVisible = function() {
+            var newPoint = document.querySelector('#'+newPoints);
+            newPoint.setAttribute("scale","1 1 1");
+            // document.getElementById("#"+newPoints).hidden = false; newPoints.setAttribute("visible", "true");
+        }
+
+        this.main = function() {
+            this.setHidden;
+            this.setSky;
+            this.setVisible;
+        }
+        // Clean up add signle addEventListener to main - 
+        this.el.addEventListener('click', this.setSky);
+        this.el.addEventListener('click', this.setHidden);
+        this.el.addEventListener('click', this.setVisible);
 
     },
     update: function() {},
-    remove: function() {}
+    remove: function() {
+        this.el.removeEventListener('click', this.setSky);
+        this.el.removeEventListener('click', this.setHidden);
+        this.el.removeEventListener('click', this.setVisible);
+    }
 })
