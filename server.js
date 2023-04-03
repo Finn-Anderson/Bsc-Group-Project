8265ce10c1fe4e5f9576f820ad9f91ae6c8e572d
FereWulf <finn@toxicflames.co.uk>
2023-04-03 22:49:14 +0100

    Setup javascript to get img data and hotpoints from server
    img retained so that I can copy src in future

--- server.js
+++ server.js
@@ -6,12 +6,31 @@ var express = require("express");
 var app = express();
 app.use(express.static(__dirname + "/src")); //use static files in ROOT/src folder
 
-app.get("/", function(request, response){ //root dir
+app.get("/", function(request, response) { //root dir
 	response.sendFile(__dirname + "/src/index.html");
 })
 
-app.get("/requestdata", function(request, response){ //root dir
-	response.send("data");
+app.get("/requestdata", function(request, response) {
+	var obj = new Object();
+	obj.img = request.query.loc;
+
+	if (request.query.loc == "img/Front-Entrance/GS__0107.JPG") {
+		obj.hpImg = ["img/Front-Entrance/GS__0106.JPG"];
+		obj.hpPos = ["12 0 -10"];
+		obj.hpRot = ["0 -45 0"];
+	} else if (request.query.loc == "img/Front-Entrance/GS__0106.JPG") {
+		obj.hpImg = ["img/Front-Entrance/GS__0107.JPG", "img/Front-Entrance/GS__0105.JPG"];
+		obj.hpPos = ["-12 0 0", "15 0 3"];
+		obj.hpRot = ["0 90 0", "0 -90 0"];
+	} else {
+		obj.hpImg = ["img/Front-Entrance/GS__0106.JPG"];
+		obj.hpPos = ["-15 0 2"];
+		obj.hpRot = ["0 90 0"];
+	}
+
+	var json = JSON.stringify(obj);
+
+	response.send(json);
 })
 
 app.listen(port, host);
--- src/classes/hotpoints.js
+++ src/classes/hotpoints.js
@@ -1,57 +1,37 @@
 /*
-    Development build version: 0.1
-    Build date: 02/03/2023
-    Author: Bsc Team Project Group 2023
-    Description: Hotpoint Navigation Components
+	Development build version: 0.1
+	Build date: 02/03/2023
+	Author: Bsc Team Project Group 2023
+	Description: Hotpoint Navigation Components
 */
 
 AFRAME.registerComponent('hotpoint', {
-    schema: {
-        room:{type:"string",default:""},
-        points: {type:"string", default:""},
-    },
+	schema: {
+		room:{type:"string",default:""},
+		points: {type:"string", default:""},
+	},
 
-    init: function() {
+	init: function() {
 
-        var newSky = this.data.room;
-        var newPoints = this.data.points;
+		var newSky = this.data.room;
+		var newPoints = this.data.points;
 
-        this.setSky = function() {
-            var sky = document.querySelector('#img-360');
-            sky.setAttribute("src", newSky);
-        }
+		this.setSky = function() {
+			var sky = document.querySelector('#img-360');
+			sky.setAttribute("src", newSky);
 
-        this.setHidden = function() {
-            var current_hotpoint = this.parentElement.getAttribute("id");
-            var hotpoint = document.querySelector('#'+current_hotpoint);
-            hotpoint.setAttribute("scale","0 0 0");
-            console.log(current_hotpoint);
-        }
+			getData(newSky);
+		}
 
-        this.setVisible = function() {
-            var newPoint = document.querySelector('#'+newPoints);
-            newPoint.setAttribute("scale","1 1 1");
-            // document.getElementById("#"+newPoints).hidden = false; newPoints.setAttribute("visible", "true");
-        }
+		this.main = function() {
+			this.setSky;
+		}
+		// Clean up add signle addEventListener to main - 
+		this.el.addEventListener('click', this.setSky);
 
-        this.main = function() {
-            this.setHidden;
-            this.setSky;
-            this.setVisible;
-        }
-        // Clean up add signle addEventListener to main - 
-        this.el.addEventListener('click', this.setSky);
-        this.el.addEventListener('click', this.setHidden);
-        this.el.addEventListener('click', this.setVisible);
-
-    },
-    update: function() {},
-    remove: function() {
-        this.el.removeEventListener('click', this.setSky);
-        this.el.removeEventListener('click', this.setHidden);
-        this.el.removeEventListener('click', this.setVisible);
-    }
+	},
+	update: function() {},
+	remove: function() {
+		this.el.removeEventListener('click', this.setSky);
+	}
 })
-
-})
-
