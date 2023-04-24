237e678965da72db314357856ae369de66fd512a
BscPaul <114180827+BscPaul@users.noreply.github.com>
2023-04-24 12:28:16 +0100

    Update menus.js

--- src/classes/menus.js
+++ src/classes/menus.js
@@ -1,1 +1,83 @@
 
+/*
+    Development build version: 0.1
+    Build date: 02/03/2023
+    Author: Bsc Team Project Group 2023
+    Description: Hotpoint Navigation Components
+*/
+
+AFRAME.registerComponent('menu', {
+    schema: {
+        title:{type:"string",default:""},
+        description: {type:"string", default:""},
+    },
+
+    init: function() {
+
+        var title = this.data.title;
+        var description = this.data.description;
+        var desc_background = "#informationpointBackground";
+        var hotpointfile = "#informationpoint";
+        
+        console.log(this.el.getAttribute("position"));
+
+        function setInformation(current_information_point) {
+            // Replace Hotpoint Icon for information box
+            var hotpoint = document.querySelector('#'+current_information_point);
+            hotpoint.setAttribute("src", desc_background);
+            hotpoint.setAttribute("scale", "10 10 10");
+
+            // Get child text elements
+            var childs = hotpoint.querySelectorAll('a-text');
+            
+            // Access childs
+            childs.forEach(function(childE) {
+
+                // Update child elements attributes
+                if (childE.getAttribute("id") == "title") {
+                    childE.setAttribute('scale', "1 1 1");
+                    childE.setAttribute('value', title);
+                } else if (childE.getAttribute("id") == "desc") {
+                    childE.setAttribute('scale', "1 1 1");
+                    childE.setAttribute('value', description);
+                }
+            });
+        }
+
+        function setInformationIcon(current_information_point) {
+            // Set this element to Icon
+            var hotpoint = document.querySelector('#'+current_information_point);
+            hotpoint.setAttribute("src", hotpointfile);
+            hotpoint.setAttribute("scale", "1 1 1");
+
+            // Get child text elements
+            var childs = hotpoint.querySelectorAll('a-text');
+            
+            // Access childs
+            childs.forEach(function(childE) {
+                childE.setAttribute('scale', "0 0 0");
+            });
+
+        }
+
+        this.setMenu = function() {
+
+            var current_information_point = this.getAttribute("src");
+
+            var element = this.getAttribute("id");
+
+            if (current_information_point === desc_background) {
+                setInformationIcon(element);
+
+            } else {
+                var current_information_point = this.getAttribute("id");
+                setInformation(element);
+            }
+        }
+        this.el.addEventListener('click', this.setMenu);
+    },
+    update: function() {},
+    remove: function() {
+        this.el.removeEventListener('click', this.setMenu);
+    }
+})
